0cb0473e575c161e9e0be9a8923c0305f1f37de7
dimarising <61971665+dimarising@users.noreply.github.com>
2020-03-26 19:35:34 +0100

    Add files via upload

---
+++ CW4/obj/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW4
+{
+    public class Student
+    {
+        public string IndexNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string BIRTHDATE { get; set; }
+
+       
+    }
+}
